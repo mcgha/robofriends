0dd7c130ba34e0c7e0b540a98b431141e2e9d321
Marcos Campos <marcoscampos@Marcoss-MacBook-Pro.local>
2020-10-03 14:37:07 -0400

    fixed height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', broder: '1px solid black'}}>
+        <div style={{overflowY: 'scroll', broder: '1px solid black', height: '800px'}}>
             {props.children}
         </div>
     )
